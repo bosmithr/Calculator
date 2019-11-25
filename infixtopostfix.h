4d6461691c18112dc77d30e87746b998a28f989d
TingtingX13 <55857311+TingtingX13@users.noreply.github.com>
2019-11-25 12:10:54 +0000

    Add files via upload

---
+++ infixtopostfix.h
@@ -0,0 +1,27 @@
+/*
+    infix2postfix.c
+    Tingting Xun
+    24 November 2019
+*/
+
+#include <stdio.h>
+#include<stdlib.h> /* for exit() */
+#include <assert.h>
+#include <ctype.h> /* for isdigit(char ) */
+
+#ifndef INFIXTOPOSTFIX_H
+#define INFIXTOPOSTFIX_H
+
+#define SIZE 100
+	
+	void push(char item);
+	char pop();
+	char readchar(FILE *myfile);
+	void writechar(FILE *newfile, char c);
+	int is_operator(char symbol);
+	int precedence(char symbol);
+	void infix2postfix(FILE *myfile, FILE *newfile);
+	void testinfixtopostfix();
+
+#endif
+
